e2e7e7d8071bac10c49b59f4dbd3648208a9b3e7
19941 <19941128o@gmail.com>
2024-05-23 14:50:57 +0900

    commit

---
+++ src/main/java/me/hugh/springbootdeveloper/SpringbootDeveloperApplication.java
@@ -0,0 +1,12 @@
+package me.hugh.springbootdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDeveloperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDeveloperApplication.class, args);
+    }
+}
